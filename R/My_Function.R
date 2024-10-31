c5b3711a885817a5d67add0472bc74b77c75edfb
Ben Schuster <benjamin.schuster@selu.edu>
2024-10-31 19:07:59 +0000

    R

--- R/My_Function.R
+++ R/My_Function.R
@@ -1,14 +1,21 @@
+#' Checks to see if fish in selected column are too small
+#'
+#' @param filepath The path to the file you want to process
+#' @param columns The selected column you want to use in the function
+#'
+#' @return column indicating fish that are too small. Will print "Check small value"
+#' @export
 my_function <- function(filepath, columns){
   my_function_modified <- filepath %>%
-    filter(!is.na( {{ columns }} )) %>% 
-    pull({{columns}}) 
-  
+    filter(!is.na( {{ columns }} )) %>%
+    pull({{columns}})
+
   results <- (my_function_modified) / 2
   for (result in results){
     if (result < 25){
       message(paste("Check small value:", result))
     }
   }
-  
+
   return(results)
 }
